2224bd88bca5e4eed8e7b1f4cf362c7708230333
antonstaana <54714576+antonstaana@users.noreply.github.com>
2021-01-15 03:42:41 +0800

    Bug Fixes + Minor CSS Adjustments
    1. Fixed issue: Page hang when input 10,000 character for login
    2. Fixed issue: Registering restaurant category not informative
    3. Fixed issue: Registering with Long Inputs username display webpage
    4. Minor CSS adjustments on profile page

--- validators.js
+++ validators.js
@@ -1,10 +1,15 @@
 const {body} = require('express-validator');
 
+const PASSWORD_MIN = 8;
+const PASSWORD_MAX = 255;
+
 const registerCustomerValidation = [
   body('email').not().isEmpty().withMessage("Email is required."),
   body('username').not().isEmpty().withMessage("Username is required."),
-  body('password').isLength({ min: 8 }).withMessage("Password must be at least 8 characters long."),
-  body('password2').isLength({ min: 8 }).withMessage("Password must be at least 8 characters long.")
+  body('password').isLength({ min: PASSWORD_MIN }).withMessage("Password must be at least " + PASSWORD_MIN " characters long.")
+    .isLength({ max: PASSWORD_MAX }).withMessage("Password must be less than " + PASSWORD_MAX + " characters long."),
+  body('password2').isLength({ min: PASSWORD_MIN }).withMessage("Password must be at least " + PASSWORD_MIN " characters long.")
+    .isLength({ max: PASSWORD_MAX }).withMessage("Password must be less than " + PASSWORD_MAX + " characters long.")
     .custom((value, { req }) => {
       if (value !== req.body.password) {
         throw new Error("Passwords must match.");
@@ -25,8 +30,10 @@ const registerRestaurantValidation = [
   body('street').not().isEmpty().withMessage("Street is required."),
   body('city_province').not().isEmpty().withMessage("city_province is required."),
   body('contactno').not().isEmpty().withMessage("Enter Contact No.").isLength({min : 11}).withMessage("Enter valid contact number"),
-  body('password').isLength({ min: 8 }).withMessage("Password must be at least 8 characters long."),
-  body('password2').isLength({ min: 8 }).withMessage("Password must be at least 8 characters long.")
+  body('password').isLength({ min: PASSWORD_MIN }).withMessage("Password must be at least " + PASSWORD_MIN " characters long.")
+    .isLength({ max: PASSWORD_MAX }).withMessage("Password must be less than " + PASSWORD_MAX + " characters long."),
+  body('password2').isLength({ min: PASSWORD_MIN }).withMessage("Password must be at least " + PASSWORD_MIN " characters long.")
+    .isLength({ max: PASSWORD_MAX }).withMessage("Password must be less than " + PASSWORD_MAX + " characters long.")
     .custom((value, { req }) => {
       if (value !== req.body.password) {
         throw new Error("Passwords must match.");
