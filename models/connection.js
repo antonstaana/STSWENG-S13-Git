1270fe4836fbe3f4f15ce9f91fabedc83ab95933
antonstaana <54714576+antonstaana@users.noreply.github.com>
2021-02-05 05:27:39 +0800

    Update code for deployment

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
+const { dbURL } = require('../config');
 
-const databaseURL = 'mongodb://localhost:27017/app';
+//LocalHost
+//const databaseURL = 'mongodb://localhost:27017/app';
+
+//Ranjo's DB
 //'mongodb+srv://JRanjo:PC1H7qgqj0zn78oh@localeats.yuxqg.mongodb.net/LocalEats?authSource=admin&replicaSet=atlas-132e5i-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true';
+
+//Sta. Ana's DB
+//const databaseURL = 'mongodb+srv://dbAdmin:2c4AEhlRGVgc36aN@stsweng-localeats.lyrtw.mongodb.net/localeatsdb?retryWrites=true&w=majority'
 
 const options = {
   useNewUrlParser: true,
@@ -9,7 +16,8 @@ const options = {
   useFindAndModify: true
 };
 
-mongoose.connect(databaseURL, options);
+//mongoose.connect(databaseURL, options);
+mongoose.connect(dbURL, options);
 
 //Dropping DB
 /*
